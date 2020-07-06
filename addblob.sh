ca04a8113b775d965d9ce6ade205700cb6d3f970
Pierre Oblin <pierre.oblin@orange.com>
2020-07-06 20:43:36 +0000

    bump blob 1.0.10

--- addblob.sh
+++ addblob.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/rancher/rke/releases/download/v1.0.4/rke_linux-amd64 -O src/rke_linux-amd64
+wget https://github.com/rancher/rke/releases/download/v1.0.10/rke_linux-amd64 -O src/rke_linux-amd64
 bosh add-blob src/rke_linux-amd64 rke/rke
 
